66b8605c8d28732d6fec912f75f2820ecd6f1bd5
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-07-21 21:34:17 -0500

    Fix betty of advanced task 103

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -4,27 +4,28 @@
  * binary_tree_rotate_left - Performs a left-rotation on a binary tree
  * @tree: pointer to the root node of the tree to rotate
  *
- * Return: pointer to the new root node of the tree once rotated
+ * Return: A pointer to the new root node of the tree once rotated.
+ * Otherwise, NULL
  */
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_root = NULL;
+	binary_tree_t *tmp_root = NULL;
 
 	if (!tree || !(tree->right))
 		return (NULL);
 
-	new_root = tree->right;
-	tree->parent = new_root;
-	if (new_root->left)
+	tmp_root = tree->right;
+	tree->parent = tmp_root;
+	if (tmp_root->left)
 	{
-		tree->right = new_root->left;
-		new_root->left->parent = tree;
+		tree->right = tmp_root->left;
+		tmp_root->left->parent = tree;
 	}
 	else
 		tree->right =  NULL;
-	new_root->left = tree;
-	new_root->parent = NULL;
+	tmp_root->left = tree;
+	tmp_root->parent = NULL;
 
-	return (new_root);
+	return (tmp_root);
 }
